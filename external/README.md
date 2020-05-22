486e2cef0367644986cb51c5433ae9f0b646042c
Julian Bühler <juli27@users.noreply.github.com>
2020-05-22 22:16:45 +0200

    [external] Update spdlog (1.3.1 -> 1.4.1)
    Part of #13

--- external/README.md
+++ external/README.md
@@ -1,3 +1,4 @@
-* Dear ImGui (1.71)
-* fmt (5.3.0)
-* spdlog (1.3.1)
+* Dear ImGui (1.72b)
+* EnTT (v3.1.1)
+* fmt (6.0.0)
+* spdlog (v1.4.1)
--- runtime/runtime/shared/Log.cpp
+++ runtime/runtime/shared/Log.cpp
@@ -2,6 +2,7 @@
 
 #include "runtime/platform/Platform.h"
 
+#include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
 
 #if BASALT_DEV_BUILD
