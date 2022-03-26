4fae9f5c583458462e0edb2ae7a9ceebe18256e5
Julian Bühler <juli27@users.noreply.github.com>
2022-03-26 22:14:40 +0100

    build: Cleanup cmake scripts

---
+++ cmake/BasaltApp.cmake
@@ -0,0 +1,51 @@
+function(basalt_add_app APP_NAME)
+  add_library(${APP_NAME} STATIC)
+  set(CURRENT_TARGET ${APP_NAME} PARENT_SCOPE)
+
+  target_link_libraries(${APP_NAME} PUBLIC Basalt::LibAPI)
+
+  set_target_properties(${APP_NAME} PROPERTIES
+    FOLDER ${APP_NAME}
+  )
+
+  set(EXE_NAME "${APP_NAME}.Win32")
+  add_executable(${EXE_NAME} WIN32)
+  set(CURRENT_TARGET ${EXE_NAME})
+
+  target_link_libraries(${CURRENT_TARGET} PRIVATE
+    ControlFlowGuard
+    CommonPrivate
+    WarningsAsErrors
+    ${APP_NAME}
+    Basalt::LibAPI
+    Basalt::LibRuntime
+    Basalt::LibWin32
+  )
+
+  target_compile_definitions(${CURRENT_TARGET} PRIVATE
+    "$<$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>:BASALT_DEV_BUILD>"
+    "$<$<CONFIG:Debug>:BASALT_DEBUG_BUILD>"
+  )
+
+  # TODO: /LTCG:incremental with ninja
+  target_link_options(${CURRENT_TARGET} PRIVATE
+    "/WX"
+    "$<$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>:/DEBUG:FASTLINK>"
+    "$<$<NOT:$<CONFIG:Debug>>:/OPT:REF;/OPT:ICF>"
+    "$<$<CONFIG:Release>:/DEBUG:FULL>"
+  )
+
+  set_target_properties(${CURRENT_TARGET} PROPERTIES
+    FOLDER ${APP_NAME}
+    VS_DEBUGGER_WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+    VS_JUST_MY_CODE_DEBUGGING "$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>"
+  )
+
+  target_sources(${CURRENT_TARGET} PRIVATE
+    "${CMAKE_SOURCE_DIR}/launchers/win32/App.manifest"
+    "${CMAKE_SOURCE_DIR}/launchers/win32/main.cpp"
+  )
+
+  get_target_property(TARGET_SOURCES ${CURRENT_TARGET} SOURCES)
+  source_group(TREE "${CMAKE_SOURCE_DIR}/launchers/win32" FILES ${TARGET_SOURCES})
+endfunction()
---
+++ cmake/Compiler.cmake
@@ -0,0 +1,19 @@
+add_library(ControlFlowGuard INTERFACE)
+target_compile_options(ControlFlowGuard INTERFACE "/guard:cf")
+target_link_options(ControlFlowGuard INTERFACE "/GUARD:CF")
+
+add_library(Cpp17_Strict INTERFACE)
+target_compile_features(Cpp17_Strict INTERFACE cxx_std_17)
+target_compile_options(Cpp17_Strict INTERFACE "/permissive-" "/Zc:inline")
+
+add_library(WarningsAsErrors INTERFACE)
+target_compile_options(WarningsAsErrors INTERFACE "/WX")
+
+add_library(CommonPrivate INTERFACE)
+target_compile_definitions(CommonPrivate INTERFACE "_UNICODE" "UNICODE")
+target_compile_options(CommonPrivate INTERFACE
+  "/FC" "/fp:fast" "/Gy" "/sdl" "/W4"
+  "$<$<CONFIG:Debug>:/GF>"
+  "$<$<CONFIG:RelWithDebInfo>:/Ob2>"
+  "$<$<CONFIG:Release>:/Zi>"
+)
