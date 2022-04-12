8bf0d0589526c14b770b7e593870c512939bdc57
Julian Bühler <juli27@users.noreply.github.com>
2022-04-12 14:27:04 +0200

    math: Add Plane

---
+++ libapi/basalt/api/math/plane.cpp
@@ -0,0 +1,31 @@
+#include <basalt/api/math/plane.h>
+
+namespace basalt {
+
+// TODO: plane transformation
+
+// TODO: replace dot_normal(), dot_point() with a better API
+// for determining the location of a point relative to a plane
+// * enum with inside, in front, behind values
+// * angle of a normal vector relative to the plane
+
+auto Plane::from_general_form(const f32 a, const f32 b, const f32 c,
+                              const f32 d) -> Plane {
+  const Vector3f32 normal {a, b, c};
+  const f32 normalLength {normal.length()};
+
+  return Plane {normal / normalLength, d / normalLength};
+}
+
+auto Plane::from_point_normal(const Vector3f32& p, const Vector3f32& n)
+  -> Plane {
+  return from_general_form(n.x(), n.y(), n.z(), -Vector3f32::dot(p, n));
+}
+
+// TODO: review normal orientation
+auto Plane::from_points(const Vector3f32& p1, const Vector3f32& p2,
+                        const Vector3f32& p3) -> Plane {
+  return from_point_normal(p1, Vector3f32::cross(p3 - p2, p1 - p2));
+}
+
+} // namespace basalt
---
+++ libapi/basalt/api/math/plane.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <basalt/api/math/vector3.h>
+
+#include <basalt/api/base/types.h>
+
+namespace basalt {
+
+// plane in R^3 is defined by a point p and a normal vector n = (a,b,c) and can
+// be described with n * (x - p) = 0 and with d = -(p * n) => x * n + d = 0
+struct Plane final {
+  [[nodiscard]] static auto from_general_form(f32 a, f32 b, f32 c, f32 d)
+    -> Plane;
+
+  [[nodiscard]] static auto from_point_normal(const Vector3f32& p,
+                                              const Vector3f32& n) -> Plane;
+
+  [[nodiscard]] static auto from_points(const Vector3f32& p1,
+                                        const Vector3f32& p2,
+                                        const Vector3f32& p3) -> Plane;
+
+  [[nodiscard]] constexpr auto operator==(const Plane& rhs) const -> bool {
+    return mNormal == rhs.mNormal && mD == rhs.mD;
+  }
+
+  [[nodiscard]] constexpr auto operator!=(const Plane& rhs) const -> bool {
+    return !(*this == rhs);
+  }
+
+  [[nodiscard]] constexpr auto normal() const -> const Vector3f32& {
+    return mNormal;
+  }
+
+  [[nodiscard]] constexpr auto d() const -> f32 {
+    return mD;
+  }
+
+  [[nodiscard]] constexpr auto dot_normal(const Vector3f32& v) const -> f32 {
+    return Vector3f32::dot(mNormal, v);
+  }
+
+  [[nodiscard]] constexpr auto dot_coords(const Vector3f32& v) const -> f32 {
+    return dot_normal(v) + mD;
+  }
+
+  // sign-preserving. negative when behind plane
+  [[nodiscard]] constexpr auto distance(const Vector3f32& x) const -> f32 {
+    return dot_coords(x);
+  }
+
+private:
+  Vector3f32 mNormal;
+  f32 mD {};
+
+  constexpr Plane(const Vector3f32& n, const f32 d) : mNormal {n}, mD {d} {
+  }
+};
+
+} // namespace basalt
