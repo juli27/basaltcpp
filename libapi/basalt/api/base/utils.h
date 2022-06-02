25333d798ffa21fdc2ff2c7bdb98a3c5c94563ad
Julian Bühler <juli27@users.noreply.github.com>
2022-06-02 13:32:14 +0200

    base: Add saturated_cast

--- libapi/basalt/api/base/utils.h
+++ libapi/basalt/api/base/utils.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+#include <limits>
 #include <type_traits>
 
 namespace basalt {
@@ -7,6 +9,15 @@ namespace basalt {
 template <typename E, std::enable_if_t<std::is_enum_v<E>, int> = 0>
 constexpr auto enum_cast(const E e) noexcept {
   return static_cast<std::underlying_type_t<E>>(e);
+}
+
+// TODO: optimize unsigned -> unsigned conversion (same min value)
+template <typename T, typename U>
+constexpr auto saturated_cast(const U value) -> T {
+  using Limits = std::numeric_limits<T>;
+
+  return static_cast<T>(
+    std::clamp(value, U {Limits::min()}, U {Limits::max()}));
 }
 
 } // namespace basalt
