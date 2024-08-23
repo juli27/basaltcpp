850cf07f29e93928b7bce4fc29915fac3fbf2323
Julian Bühler <juli27@users.noreply.github.com>
2024-08-23 14:49:17 +0200

    Cleanup GfxSystem

--- libruntime/basalt/gfx/gfx_system.cpp
+++ libruntime/basalt/gfx/gfx_system.cpp
@@ -31,24 +31,16 @@ namespace basalt::gfx {
 
 namespace {
 
-auto record_camera(FilteringCommandList& cmdList, Scene& scene,
-                   f32 const aspectRatio) -> void {
-  auto& entities = scene.entity_registry();
-  auto const cameraEntity = CameraEntity{
-    scene.get_handle(entities.ctx().get<EntityId>(GfxSystem::sMainCamera))};
-
-  cameraEntity.get_camera().aspectRatio = aspectRatio;
-
+auto record_camera(FilteringCommandList& cmdList,
+                   CameraEntity const& cameraEntity) -> void {
   cmdList.set_transform(TransformState::ViewToClip,
                         cameraEntity.view_to_clip());
   cmdList.set_transform(TransformState::WorldToView,
                         cameraEntity.world_to_view());
 }
 
-auto record_material(FilteringCommandList& cmdList, Context const& ctx,
-                     MaterialHandle const id) -> void {
-  auto const& data = ctx.get(id);
-
+auto record_material(FilteringCommandList& cmdList, MaterialData const& data)
+  -> void {
   cmdList.bind_pipeline(data.pipeline);
   cmdList.bind_texture(0, data.texture);
   cmdList.bind_sampler(0, data.sampler);
@@ -57,24 +49,6 @@ auto record_material(FilteringCommandList& cmdList, Context const& ctx,
                        data.specularPower);
   cmdList.set_fog_parameters(data.fogColor, data.fogStart, data.fogEnd,
                              data.fogDensity);
-}
-
-auto record_render_component(FilteringCommandList& cmdList, Context const& ctx,
-                             LocalToWorld const& localToWorld,
-                             RenderComponent const& renderComponent) {
-  record_material(cmdList, ctx, renderComponent.material);
-
-  cmdList.set_transform(TransformState::LocalToWorld, localToWorld.matrix);
-
-  auto const& meshData = ctx.get(renderComponent.mesh);
-  cmdList.bind_vertex_buffer(meshData.vertexBuffer);
-
-  if (meshData.indexBuffer) {
-    cmdList.bind_index_buffer(meshData.indexBuffer);
-    cmdList.draw_indexed(0, 0, meshData.vertexCount, 0, meshData.indexCount);
-  } else {
-    cmdList.draw(meshData.startVertex, meshData.vertexCount);
-  }
 }
 
 } // namespace
@@ -87,44 +61,49 @@ auto GfxSystem::on_update(UpdateContext const& ctx) -> void {
   auto const& drawCtx = ecsCtx.get<View::DrawContext const>();
   auto const& gfxCtx = ecsCtx.get<Context const>();
   auto const& env = ecsCtx.get<Environment const>();
+  auto const cameraEntityId = ecsCtx.get<EntityId>(GfxSystem::sMainCamera);
 
   auto cmdList = FilteringCommandList{};
   cmdList.clear_attachments(
     Attachments{Attachment::RenderTarget, Attachment::DepthBuffer},
     env.background(), 1.0f);
 
-  auto const aspectRatio = drawCtx.viewport.aspect_ratio();
-  record_camera(cmdList, scene, aspectRatio);
+  auto const cameraEntity = CameraEntity{scene.get_handle(cameraEntityId)};
+  cameraEntity.get_camera().aspectRatio = drawCtx.viewport.aspect_ratio();
+  record_camera(cmdList, cameraEntity);
+
+  auto lights = [&] {
+    auto lights = vector<LightData>{};
+    auto const directionalLights = env.directional_lights();
+    if (!directionalLights.empty()) {
+      lights.insert(lights.end(), directionalLights.begin(),
+                    directionalLights.end());
+    }
+
+    // TODO: this should use LocalToWorld
+    entities.view<Transform const, Light const>().each(
+      [&](Transform const& transform, Light const& light) {
+        std::visit(Overloaded{
+                     [&](PointLight const& l) {
+                       lights.emplace_back(PointLightData{
+                         l.diffuse, l.specular, l.ambient, transform.position,
+                         l.range, l.attenuation0, l.attenuation1,
+                         l.attenuation2});
+                     },
+                     [&](SpotLight const& l) {
+                       lights.emplace_back(SpotLightData{
+                         l.diffuse, l.specular, l.ambient, transform.position,
+                         l.direction, l.range, l.attenuation0, l.attenuation1,
+                         l.attenuation2, l.falloff, l.phi, l.theta});
+                     },
+                   },
+                   light);
+      });
+
+    return lights;
+  }();
 
   cmdList.set_ambient_light(env.ambient_light());
-
-  auto lights = vector<LightData>{};
-  auto const directionalLights = env.directional_lights();
-  if (!directionalLights.empty()) {
-    lights.insert(lights.end(), directionalLights.begin(),
-                  directionalLights.end());
-  }
-
-  // TODO: this should use LocalToWorld
-  entities.view<Transform const, Light const>().each(
-    [&](Transform const& transform, Light const& light) {
-      std::visit(Overloaded{
-                   [&](PointLight const& l) {
-            lights.emplace_back(PointLightData{
-                       l.diffuse, l.specular, l.ambient, transform.position,
-                       l.range, l.attenuation0, l.attenuation1,
-                       l.attenuation2});
-                   },
-                   [&](SpotLight const& l) {
-            lights.emplace_back(SpotLightData{
-                       l.diffuse, l.specular, l.ambient, transform.position,
-                       l.direction, l.range, l.attenuation0, l.attenuation1,
-                       l.attenuation2, l.falloff, l.phi, l.theta});
-                   },
-        },
-        light);
-    });
-
   cmdList.set_lights(lights);
 
   entities.view<LocalToWorld const, ext::XModelHandle const>().each(
@@ -133,10 +112,10 @@ auto GfxSystem::on_update(UpdateContext const& ctx) -> void {
 
       auto const& modelData = gfxCtx.get(model);
 
-      auto const numMaterials = static_cast<u32>(modelData.materials.size());
-
+      auto const numMaterials = modelData.materials.size();
       for (auto i = uSize{0}; i < numMaterials; ++i) {
-        record_material(cmdList, gfxCtx, modelData.materials[i]);
+        auto const& materialData = gfxCtx.get(modelData.materials[i]);
+        record_material(cmdList, materialData);
 
         ext::XMeshCommandEncoder::draw_x_mesh(cmdList.cmd_list(),
                                               modelData.meshes[i]);
@@ -146,7 +125,21 @@ auto GfxSystem::on_update(UpdateContext const& ctx) -> void {
   entities.view<LocalToWorld const, RenderComponent const>().each(
     [&](LocalToWorld const& localToWorld,
         RenderComponent const& renderComponent) {
-      record_render_component(cmdList, gfxCtx, localToWorld, renderComponent);
+      auto const& materialData = gfxCtx.get(renderComponent.material);
+      record_material(cmdList, materialData);
+
+      cmdList.set_transform(TransformState::LocalToWorld, localToWorld.matrix);
+
+      auto const& meshData = gfxCtx.get(renderComponent.mesh);
+      cmdList.bind_vertex_buffer(meshData.vertexBuffer);
+
+      if (meshData.indexBuffer) {
+        cmdList.bind_index_buffer(meshData.indexBuffer);
+        cmdList.draw_indexed(0, 0, meshData.vertexCount, 0,
+                             meshData.indexCount);
+      } else {
+        cmdList.draw(meshData.startVertex, meshData.vertexCount);
+      }
     });
 
   drawCtx.commandLists.push_back(cmdList.take_cmd_list());
