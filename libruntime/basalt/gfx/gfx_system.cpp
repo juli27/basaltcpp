e3576126294e950d805b4abba5f8869971aa2932
Julian Bühler <juli27@users.noreply.github.com>
2024-08-29 12:11:01 +0200

    gfx: Collect draw calls before recording them

--- libruntime/basalt/gfx/gfx_system.cpp
+++ libruntime/basalt/gfx/gfx_system.cpp
@@ -31,6 +31,19 @@ namespace basalt::gfx {
 
 namespace {
 
+struct MeshDrawCall {
+  LocalToWorld objectToScene;
+  MeshHandle mesh;
+  MaterialHandle material;
+};
+
+struct XMeshDrawCall {
+  LocalToWorld objectToScene;
+  ext::XModelHandle model;
+};
+
+using DrawCall = std::variant<MeshDrawCall, XMeshDrawCall>;
+
 auto record_camera(FilteringCommandList& cmdList,
                    CameraEntity const& cameraEntity) -> void {
   cmdList.set_transform(TransformState::ViewToClip,
@@ -58,21 +71,46 @@ auto GfxSystem::on_update(UpdateContext const& ctx) -> void {
   auto& entities = scene.entity_registry();
   auto const& ecsCtx{entities.ctx()};
 
+  auto const drawCalls = [&] {
+    auto drawCalls = vector<DrawCall>();
+
+    entities.view<LocalToWorld const, ext::XModelHandle const>().each(
+      [&](LocalToWorld const& localToWorld, ext::XModelHandle const& model) {
+        drawCalls.push_back(XMeshDrawCall{
+          localToWorld,
+          model,
+        });
+      });
+
+    entities.view<LocalToWorld const, RenderComponent const>().each(
+      [&](LocalToWorld const& localToWorld,
+          RenderComponent const& renderComponent) {
+        drawCalls.push_back(MeshDrawCall{
+          localToWorld,
+          renderComponent.mesh,
+          renderComponent.material,
+        });
+      });
+
+    return drawCalls;
+  }();
+
   auto const& drawCtx = ecsCtx.get<View::DrawContext const>();
   auto const& gfxCtx = ecsCtx.get<Context const>();
-  auto const& env = ecsCtx.get<Environment const>();
-  auto const cameraEntityId = ecsCtx.get<EntityId>(GfxSystem::sMainCamera);
 
   auto cmdList = FilteringCommandList{};
+
+  auto const& env = ecsCtx.get<Environment const>();
   cmdList.clear_attachments(
     Attachments{Attachment::RenderTarget, Attachment::DepthBuffer},
     env.background(), 1.0f);
 
+  auto const cameraEntityId = ecsCtx.get<EntityId>(GfxSystem::sMainCamera);
   auto const cameraEntity = CameraEntity{scene.get_handle(cameraEntityId)};
   cameraEntity.get_camera().aspectRatio = drawCtx.viewport.aspect_ratio();
   record_camera(cmdList, cameraEntity);
 
-  auto lights = [&] {
+  auto const lights = [&] {
     auto lights = vector<LightData>{};
     auto const directionalLights = env.directional_lights();
     if (!directionalLights.empty()) {
@@ -106,41 +144,45 @@ auto GfxSystem::on_update(UpdateContext const& ctx) -> void {
   cmdList.set_ambient_light(env.ambient_light());
   cmdList.set_lights(lights);
 
-  entities.view<LocalToWorld const, ext::XModelHandle const>().each(
-    [&](LocalToWorld const& localToWorld, ext::XModelHandle const& model) {
-      cmdList.set_transform(TransformState::LocalToWorld, localToWorld.matrix);
+  for (auto const& drawCall : drawCalls) {
+    std::visit(Overloaded{
+                 [&](MeshDrawCall const& d) {
+                   auto const& materialData = gfxCtx.get(d.material);
+                   record_material(cmdList, materialData);
 
-      auto const& modelData = gfxCtx.get(model);
+                   cmdList.set_transform(TransformState::LocalToWorld,
+                                         d.objectToScene.matrix);
 
-      auto const numMaterials = modelData.materials.size();
-      for (auto i = uSize{0}; i < numMaterials; ++i) {
-        auto const& materialData = gfxCtx.get(modelData.materials[i]);
-        record_material(cmdList, materialData);
+                   auto const& meshData = gfxCtx.get(d.mesh);
+                   cmdList.bind_vertex_buffer(meshData.vertexBuffer);
 
-        ext::XMeshCommandEncoder::draw_x_mesh(cmdList.cmd_list(),
-                                              modelData.meshes[i]);
-      }
-    });
+                   if (meshData.indexBuffer) {
+                     cmdList.bind_index_buffer(meshData.indexBuffer);
+                     cmdList.draw_indexed(0, 0, meshData.vertexCount, 0,
+                                          meshData.indexCount);
+                   } else {
+                     cmdList.draw(meshData.startVertex, meshData.vertexCount);
+                   }
+                 },
+                 [&](XMeshDrawCall const& d) {
+                   cmdList.set_transform(TransformState::LocalToWorld,
+                                         d.objectToScene.matrix);
 
-  entities.view<LocalToWorld const, RenderComponent const>().each(
-    [&](LocalToWorld const& localToWorld,
-        RenderComponent const& renderComponent) {
-      auto const& materialData = gfxCtx.get(renderComponent.material);
-      record_material(cmdList, materialData);
+                   auto const& modelData = gfxCtx.get(d.model);
 
-      cmdList.set_transform(TransformState::LocalToWorld, localToWorld.matrix);
+                   auto const numMaterials = modelData.materials.size();
+                   for (auto i = uSize{0}; i < numMaterials; ++i) {
+                     auto const& materialData =
+                       gfxCtx.get(modelData.materials[i]);
+                     record_material(cmdList, materialData);
 
-      auto const& meshData = gfxCtx.get(renderComponent.mesh);
-      cmdList.bind_vertex_buffer(meshData.vertexBuffer);
-
-      if (meshData.indexBuffer) {
-        cmdList.bind_index_buffer(meshData.indexBuffer);
-        cmdList.draw_indexed(0, 0, meshData.vertexCount, 0,
-                             meshData.indexCount);
-      } else {
-        cmdList.draw(meshData.startVertex, meshData.vertexCount);
-      }
-    });
+                     ext::XMeshCommandEncoder::draw_x_mesh(cmdList.cmd_list(),
+                                                           modelData.meshes[i]);
+                   }
+                 },
+               },
+               drawCall);
+  }
 
   drawCtx.commandLists.push_back(cmdList.take_cmd_list());
 }
