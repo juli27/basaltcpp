b3471dc0beecabde549a178b7258ac81bef747a8
Julian Bühler <juli27@users.noreply.github.com>
2024-11-23 23:14:40 +0100

    Cleanup CMake files

--- cmake/BasaltApp.cmake
+++ cmake/BasaltApp.cmake
@@ -1,18 +1,14 @@
 function(basalt_add_app APP_NAME)
   add_library(${APP_NAME} STATIC)
-  set(CURRENT_TARGET ${APP_NAME} PARENT_SCOPE)
 
   target_link_libraries(${APP_NAME} PUBLIC Basalt::LibAPI)
 
-  set_target_properties(${APP_NAME} PROPERTIES
-    FOLDER ${APP_NAME}
-  )
+  set_property(TARGET ${APP_NAME} PROPERTY FOLDER ${APP_NAME})
 
   set(EXE_NAME "${APP_NAME}.Win32")
   add_executable(${EXE_NAME} WIN32)
-  set(CURRENT_TARGET ${EXE_NAME})
 
-  target_link_libraries(${CURRENT_TARGET} PRIVATE
+  target_link_libraries(${EXE_NAME} PRIVATE
     ControlFlowGuard
     CommonPrivate
     ${APP_NAME}
@@ -21,13 +17,13 @@ function(basalt_add_app APP_NAME)
     Basalt::LibWin32
   )
 
-  target_compile_definitions(${CURRENT_TARGET} PRIVATE
+  target_compile_definitions(${EXE_NAME} PRIVATE
     "$<$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>:BASALT_DEV_BUILD>"
     "$<$<CONFIG:Debug>:BASALT_DEBUG_BUILD>"
   )
 
-  target_compile_features(${CURRENT_TARGET} PRIVATE cxx_std_17)
-  target_compile_options(${CURRENT_TARGET}
+  target_compile_features(${EXE_NAME} PRIVATE cxx_std_17)
+  target_compile_options(${EXE_NAME}
     PRIVATE
       "/w44062;/w14165;/w34191;/w44242"
       "/Zc:__cplusplus"
@@ -46,18 +42,18 @@ function(basalt_add_app APP_NAME)
   )
 
   # TODO: /LTCG:incremental with ninja
-  target_link_options(${CURRENT_TARGET} PRIVATE
+  target_link_options(${EXE_NAME} PRIVATE
     "/WX"
     "$<$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>:/DEBUG:FASTLINK>"
     "$<$<NOT:$<CONFIG:Debug>>:/OPT:REF;/OPT:ICF>"
     "$<$<CONFIG:Release>:/DEBUG:FULL>"
   )
 
-  set_target_properties(${CURRENT_TARGET} PROPERTIES
-    FOLDER ${APP_NAME}
-    VS_DEBUGGER_WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
-    VS_JUST_MY_CODE_DEBUGGING "$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>"
-  )
+  set_property(TARGET ${EXE_NAME} PROPERTY FOLDER ${APP_NAME})
+  set_property(TARGET ${EXE_NAME} PROPERTY VS_DEBUGGER_WORKING_DIRECTORY
+               "${CMAKE_SOURCE_DIR}")
+  set_property(TARGET ${EXE_NAME} PROPERTY VS_JUST_MY_CODE_DEBUGGING
+              "$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>")
 
   set(BASALT_WIN32_MANIFEST_APP_NAME "basalt.${APP_NAME}")
   configure_file("${PROJECT_SOURCE_DIR}/platformlibs/libwin32/app.manifest.in"
@@ -68,7 +64,7 @@ function(basalt_add_app APP_NAME)
     "main.cpp"
     COPYONLY)
 
-  target_sources(${CURRENT_TARGET}
+  target_sources(${EXE_NAME}
     PRIVATE
       "${CMAKE_CURRENT_BINARY_DIR}/app.manifest"
       "${CMAKE_CURRENT_BINARY_DIR}/main.cpp")
