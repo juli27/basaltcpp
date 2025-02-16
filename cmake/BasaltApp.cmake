70ec04f54b612bf8b4b2b30cbf890bbcbd1b0e7b
Julian Bühler <juli27@users.noreply.github.com>
2025-02-16 22:14:15 +0100

    infra: Update CMake build and CI

--- cmake/BasaltApp.cmake
+++ cmake/BasaltApp.cmake
@@ -1,7 +1,7 @@
 function(basalt_add_app APP_NAME)
   add_library(${APP_NAME} STATIC)
 
-  target_link_libraries(${APP_NAME} PUBLIC Basalt::LibAPI)
+  target_link_libraries(${APP_NAME} PRIVATE Basalt::LibAPI)
 
   set_property(TARGET ${APP_NAME} PROPERTY FOLDER ${APP_NAME})
 
@@ -11,14 +11,7 @@ function(basalt_add_app APP_NAME)
   target_link_libraries(${EXE_NAME} PRIVATE
     CommonFlags
     ${APP_NAME}
-    Basalt::LibAPI
-    Basalt::LibRuntime
     Basalt::LibWin32
-  )
-
-  target_compile_definitions(${EXE_NAME} PRIVATE
-    "$<$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>:BASALT_DEV_BUILD>"
-    "$<$<CONFIG:Debug>:BASALT_DEBUG_BUILD>"
   )
 
   target_compile_features(${EXE_NAME} PRIVATE cxx_std_17)
@@ -31,24 +24,30 @@ function(basalt_add_app APP_NAME)
   )
 
   set_property(TARGET ${EXE_NAME} PROPERTY FOLDER ${APP_NAME})
-  set_property(TARGET ${EXE_NAME} PROPERTY VS_DEBUGGER_WORKING_DIRECTORY
-               "${CMAKE_SOURCE_DIR}")
-  set_property(TARGET ${EXE_NAME} PROPERTY VS_JUST_MY_CODE_DEBUGGING
-              "$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>")
+  set_property(TARGET ${EXE_NAME} PROPERTY
+    VS_DEBUGGER_WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+  )
+  set_property(TARGET ${EXE_NAME} PROPERTY
+    VS_JUST_MY_CODE_DEBUGGING "$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>"
+  )
 
   set(BASALT_WIN32_MANIFEST_APP_NAME "basalt.${APP_NAME}")
   configure_file("${PROJECT_SOURCE_DIR}/platformlibs/libwin32/app.manifest.in"
     "app.manifest"
-    @ONLY)
+    @ONLY
+  )
 
   configure_file("${PROJECT_SOURCE_DIR}/platformlibs/libwin32/main.cpp"
     "main.cpp"
-    COPYONLY)
+    COPYONLY
+  )
 
   target_sources(${EXE_NAME} PRIVATE
     "${CMAKE_CURRENT_BINARY_DIR}/app.manifest"
-    "${CMAKE_CURRENT_BINARY_DIR}/main.cpp")
+    "${CMAKE_CURRENT_BINARY_DIR}/main.cpp"
+  )
   source_group("" FILES
     "${CMAKE_CURRENT_BINARY_DIR}/app.manifest"
-    "${CMAKE_CURRENT_BINARY_DIR}/main.cpp")
+    "${CMAKE_CURRENT_BINARY_DIR}/main.cpp"
+  )
 endfunction()
