fa95cfe09c0d5be0b2694ed88f16eac42a54485f
Julian Bühler <juli27@users.noreply.github.com>
2025-10-12 21:29:45 +0200

    sandbox: Cleanup tribase textures exercises

--- sandbox/basalt/sandbox/tribase/02-04_textures_exercises.cpp
+++ sandbox/basalt/sandbox/tribase/02-04_textures_exercises.cpp
@@ -1,7 +1,8 @@
-#include <basalt/sandbox/tribase/02-04_textures_exercises.h>
+#include "tribase_examples.h"
 
 #include <basalt/api/engine.h>
 #include <basalt/api/prelude.h>
+#include <basalt/api/view.h>
 
 #include <basalt/api/gfx/context.h>
 #include <basalt/api/gfx/resource_cache.h>
@@ -23,153 +24,165 @@
 #include <string_view>
 #include <utility>
 
-using namespace std::literals;
-using std::array;
-using std::byte;
-
-using gsl::span;
-
+using namespace basalt;
 using namespace basalt::literals;
-using basalt::Angle;
-using basalt::Engine;
-using basalt::Matrix4x4f32;
-using basalt::Vector2f32;
-using basalt::Vector3f32;
-using basalt::gfx::Attachment;
-using basalt::gfx::Attachments;
-using basalt::gfx::CommandList;
-using basalt::gfx::FixedFragmentShaderCreateInfo;
-using basalt::gfx::PipelineCreateInfo;
-using basalt::gfx::PrimitiveType;
-using basalt::gfx::TextureStage;
-using basalt::gfx::TransformState;
-using basalt::gfx::VertexElement;
-
-namespace tribase {
+using namespace std::literals;
 
 namespace {
 
-constexpr auto TEXTURE_FILE_PATH = "data/banana.bmp"sv;
+auto constexpr TEXTURE_FILE_PATH = "data/banana.bmp"sv;
 
-struct Vertex final {
-  Vector3f32 pos{};
-  Vector2f32 uv{};
+struct Vertex {
+  Vector3f32 pos;
+  Vector2f32 uv;
 
   static constexpr auto sLayout =
-    basalt::gfx::make_vertex_layout<VertexElement::Position3F32,
-                                    VertexElement::TextureCoords2F32>();
+    gfx::make_vertex_layout<gfx::VertexElement::Position3F32,
+                            gfx::VertexElement::TextureCoords2F32>();
 };
 
-constexpr auto TRIANGLE_VERTICES = array{
+auto constexpr TRIANGLE_VERTICES = std::array{
   Vertex{{0.0f, 0.5f, 0.0f}, {0.5f, 0.0f}},
   Vertex{{0.5f, -0.5f, 0.0f}, {1.0f, 1.0f}},
   Vertex{{-0.5f, -0.5f, 0.0f}, {0.0f, 1.0f}},
 };
 
+class TexturesExercises final : public basalt::View {
+public:
+  explicit TexturesExercises(basalt::Engine const& engine)
+    : mGfxCache{engine.create_gfx_resource_cache()}
+    , mSampler{mGfxCache->create_sampler(gfx::SamplerCreateInfo{})}
+    , mTexture{mGfxCache->load_texture_2d(TEXTURE_FILE_PATH)}
+    , mVertexBuffer{[&] {
+      auto const vertexData = as_bytes(gsl::span{TRIANGLE_VERTICES});
+      auto info = gfx::VertexBufferCreateInfo{};
+      info.sizeInBytes = vertexData.size_bytes();
+      info.layout = Vertex::sLayout;
+
+      return mGfxCache->create_vertex_buffer(info, vertexData);
+    }()}
+    , mPipeline{[&] {
+      auto fs = gfx::FixedFragmentShaderCreateInfo{};
+      constexpr auto textureStages = std::array{gfx::TextureStage{}};
+      fs.textureStages = textureStages;
+
+      auto info = gfx::PipelineCreateInfo{};
+      info.fragmentShader = &fs;
+      info.vertexLayout = Vertex::sLayout;
+      info.primitiveType = gfx::PrimitiveType::TriangleList;
+      return mGfxCache->create_pipeline(info);
+    }()} {
+  }
+
+private:
+  gfx::ResourceCachePtr mGfxCache;
+  gfx::SamplerHandle mSampler;
+  gfx::TextureHandle mTexture;
+  gfx::VertexBufferHandle mVertexBuffer;
+  gfx::PipelineHandle mPipeline;
+  i32 mUvAnimationMode{0};
+  SecondsF32 mTime{0};
+  bool mAnimateUvs{false};
+
+  auto on_update(UpdateContext& ctx) -> void override {
+    if (mAnimateUvs) {
+      mTime += ctx.deltaTime;
+    }
+
+    auto const& gfxCtx = ctx.engine.gfx_context();
+
+    if (ImGui::Begin("Settings##TribaseTexturesEx")) {
+      ImGui::Checkbox("Animate UVs", &mAnimateUvs);
+      ImGui::SameLine();
+      if (ImGui::Button("Reset")) {
+        mTime = 0s;
+        gfxCtx.with_mapping_of(
+          mVertexBuffer, [](gsl::span<std::byte> const vbData) {
+            auto const vertexData = as_bytes(gsl::span{TRIANGLE_VERTICES});
+
+            std::copy_n(vertexData.begin(),
+                        std::min(vertexData.size_bytes(), vbData.size_bytes()),
+                        vbData.begin());
+          });
+      }
+
+      ImGui::Indent();
+
+      if (ImGui::RadioButton("Move", &mUvAnimationMode, 0)) {
+        mTime = 0s;
+      }
+
+      if (ImGui::RadioButton("Rotate", &mUvAnimationMode, 1)) {
+        mTime = 0s;
+      }
+    }
+
+    ImGui::End();
+
+    auto const t = mTime.count();
+
+    if (mUvAnimationMode == 0) {
+      gfxCtx.with_mapping_of(
+        mVertexBuffer, [&](gsl::span<std::byte> const vbData) {
+          auto const vertexData =
+            gsl::span<Vertex>{reinterpret_cast<Vertex*>(vbData.data()),
+                              vbData.size() / sizeof(Vertex)};
+
+          auto const translateUv = [&](Vertex const& v) {
+            return Vertex{v.pos, Vector2f32{
+                                   v.uv.x() + 0.25f * t,
+                                   v.uv.y(),
+                                 }};
+          };
+          std::transform(TRIANGLE_VERTICES.begin(), TRIANGLE_VERTICES.end(),
+                         vertexData.begin(), translateUv);
+        });
+    } else if (mUvAnimationMode == 1) {
+      gfxCtx.with_mapping_of(
+        mVertexBuffer, [&](gsl::span<std::byte> const vbData) {
+          auto const vertexData =
+            gsl::span<Vertex>{reinterpret_cast<Vertex*>(vbData.data()),
+                              vbData.size() / sizeof(Vertex)};
+
+          auto const rotation =
+            Matrix4x4f32::rotation_z(Angle::degrees(45.0f * t));
+          auto const rotateUv = [&](Vertex const& v) {
+            return Vertex{v.pos,
+                          Vector2f32{
+                            v.uv.dot(Vector2f32{rotation.m11, rotation.m21}),
+                            v.uv.dot(Vector2f32{rotation.m12, rotation.m22}),
+                          }};
+          };
+          std::transform(TRIANGLE_VERTICES.begin(), TRIANGLE_VERTICES.end(),
+                         vertexData.begin(), rotateUv);
+        });
+    }
+
+    auto cmdList = gfx::CommandList{};
+    cmdList.clear_attachments(gfx::Attachments{gfx::Attachment::RenderTarget},
+                              Color::from_non_linear(0.103f, 0.103f, 0.103f));
+    cmdList.bind_pipeline(mPipeline);
+    cmdList.bind_sampler(0, mSampler);
+    cmdList.bind_texture(0, mTexture);
+
+    auto const& drawCtx = ctx.drawCtx;
+    auto const aspectRatio = drawCtx.viewport.aspect_ratio();
+    cmdList.set_transform(
+      gfx::TransformState::ViewToClip,
+      Matrix4x4f32::perspective_projection(90_deg, aspectRatio, 0.1f, 100.0f));
+    cmdList.set_transform(gfx::TransformState::WorldToView,
+                          Matrix4x4f32::identity());
+    cmdList.set_transform(gfx::TransformState::LocalToWorld,
+                          Matrix4x4f32::translation(0.0f, 0.0f, 1.0f));
+    cmdList.bind_vertex_buffer(mVertexBuffer);
+    cmdList.draw(0, 3);
+
+    drawCtx.commandLists.push_back(std::move(cmdList));
+  }
+};
+
 } // namespace
 
-TexturesExercises::TexturesExercises(Engine const& engine)
-  : mGfxCache{engine.create_gfx_resource_cache()}
-  , mSampler{mGfxCache->create_sampler({})}
-  , mTexture{mGfxCache->load_texture_2d(TEXTURE_FILE_PATH)}
-  , mVertexBuffer{[&] {
-    auto const vertexData = as_bytes(span{TRIANGLE_VERTICES});
-
-    return mGfxCache->create_vertex_buffer(
-      {vertexData.size_bytes(), Vertex::sLayout}, vertexData);
-  }()}
-  , mPipeline{[&] {
-    auto fs = FixedFragmentShaderCreateInfo{};
-    constexpr auto textureStages = array{TextureStage{}};
-    fs.textureStages = textureStages;
-
-    auto desc = PipelineCreateInfo{};
-    desc.fragmentShader = &fs;
-    desc.vertexLayout = Vertex::sLayout;
-    desc.primitiveType = PrimitiveType::TriangleList;
-    return mGfxCache->create_pipeline(desc);
-  }()} {
+auto TribaseExamples::new_02_04_textures_exercises(Engine& engine) -> ViewPtr {
+  return std::make_shared<TexturesExercises>(engine);
 }
-
-auto TexturesExercises::on_update(UpdateContext& ctx) -> void {
-  auto const& gfxCtx = ctx.engine.gfx_context();
-
-  if (ImGui::Begin("Settings##TribaseTexturesEx")) {
-    auto uploadTriangle = [&] {
-      gfxCtx.with_mapping_of(mVertexBuffer, [](span<byte> const vbData) {
-        auto const vertexData = as_bytes(span{TRIANGLE_VERTICES});
-
-        std::copy_n(vertexData.begin(),
-                    std::min(vertexData.size_bytes(), vbData.size_bytes()),
-                    vbData.begin());
-      });
-    };
-
-    if (ImGui::RadioButton("Exercise 1", &mCurrentExercise, 0)) {
-      uploadTriangle();
-    }
-
-    if (ImGui::RadioButton("Exercise 2", &mCurrentExercise, 1)) {
-      uploadTriangle();
-    }
-
-    if (ImGui::RadioButton("Exercise 3", &mCurrentExercise, 2)) {
-      uploadTriangle();
-    }
-  }
-
-  ImGui::End();
-
-  auto const dt = ctx.deltaTime.count();
-
-  if (mCurrentExercise == 1) {
-    gfxCtx.with_mapping_of(mVertexBuffer, [&](span<byte> const vbData) {
-      auto const vertexData =
-        span<Vertex>{reinterpret_cast<Vertex*>(vbData.data()),
-                     vbData.size() / sizeof(Vertex)};
-
-      // READING from the vertex buffer!
-      for (auto& vertex : vertexData) {
-        vertex.uv.x() += 0.25f * dt;
-      }
-    });
-  }
-
-  if (mCurrentExercise == 2) {
-    gfxCtx.with_mapping_of(mVertexBuffer, [&](span<byte> const vbData) {
-      auto const vertexData =
-        span<Vertex>{reinterpret_cast<Vertex*>(vbData.data()),
-                     vbData.size() / sizeof(Vertex)};
-
-      auto const rotation =
-        Matrix4x4f32::rotation_z(Angle::degrees(45.0f * dt));
-      // READING from the vertex buffer!
-      for (auto& [pos, uv] : vertexData) {
-        uv = {uv.dot(Vector2f32{rotation.m11, rotation.m21}),
-              uv.dot(Vector2f32{rotation.m12, rotation.m22})};
-      }
-    });
-  }
-
-  auto cmdList = CommandList{};
-  cmdList.clear_attachments(Attachments{Attachment::RenderTarget},
-                            Color::from_non_linear(0.103f, 0.103f, 0.103f));
-  cmdList.bind_pipeline(mPipeline);
-  cmdList.bind_sampler(0, mSampler);
-  cmdList.bind_texture(0, mTexture);
-
-  auto const& drawCtx = ctx.drawCtx;
-  auto const aspectRatio = drawCtx.viewport.aspect_ratio();
-  cmdList.set_transform(
-    TransformState::ViewToClip,
-    Matrix4x4f32::perspective_projection(90_deg, aspectRatio, 0.1f, 100.0f));
-  cmdList.set_transform(TransformState::WorldToView, Matrix4x4f32::identity());
-  cmdList.set_transform(TransformState::LocalToWorld,
-                        Matrix4x4f32::translation(0.0f, 0.0f, 1.0f));
-  cmdList.bind_vertex_buffer(mVertexBuffer);
-  cmdList.draw(0, 3);
-
-  drawCtx.commandLists.emplace_back(std::move(cmdList));
-}
-
-} // namespace tribase
--- sandbox/basalt/sandbox/tribase/tribase_examples.cpp
+++ sandbox/basalt/sandbox/tribase/tribase_examples.cpp
@@ -1,6 +1,5 @@
 #include <basalt/sandbox/tribase/tribase_examples.h>
 
-#include <basalt/sandbox/tribase/02-04_textures_exercises.h>
 #include <basalt/sandbox/tribase/02-05_buffers.h>
 #include <basalt/sandbox/tribase/02-05_buffers_exercises.h>
 #include <basalt/sandbox/tribase/02-06_fog.h>
@@ -14,11 +13,6 @@
 #include <basalt/sandbox/tribase/02-14_effects.h>
 
 #include <memory>
-
-auto TribaseExamples::new_02_04_textures_exercises(basalt::Engine& engine)
-  -> basalt::ViewPtr {
-  return std::make_shared<tribase::TexturesExercises>(engine);
-}
 
 auto TribaseExamples::new_02_05_buffers_example(basalt::Engine& engine)
   -> basalt::ViewPtr {
